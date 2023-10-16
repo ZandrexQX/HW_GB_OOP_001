8fc740e7eb03238888725c39db64f8ab463fac6c
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-10-16 06:18:53 +0300

    Добавьте файлы проекта.

---
+++ HW_GB_OOP_001/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HW_GB_OOP_001
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class FamilyMember
+    {
+        public FamilyMember()
+        {
+            this.Mother = null;
+            this.Father = null;
+        }
+        public FamilyMember(string name, Gender sex)
+        {
+            this.Mother = null;
+            this.Father = null;
+            this.Name = name;
+            this.Sex = sex;
+        }
+
+        public FamilyMember(string name, Gender sex, FamilyMember mother, FamilyMember father)
+        {
+            this.Mother = mother;
+            this.Father = father;
+            this.Name = name;
+            this.Sex = sex;
+        }
+        internal FamilyMember Mother { get; set; }
+        internal FamilyMember Father { get; set; }
+        internal string Name { get; set; }
+        internal Gender Sex { get; set; }
+
+        public virtual void Info(int indent = 0)
+        {
+            Console.WriteLine($"{new String('-', indent)}Имя {this.Name}");
+        }
+        public virtual void Print(int indent = 0)
+        {
+            Info(indent);
+        }
+
+
+    }
+
+    public class AdultFamilyMember : FamilyMember
+    {
+        public AdultFamilyMember()
+        {
+            this.Mother = null;
+            this.Father = null;
+        }
+        public AdultFamilyMember(string name, Gender sex)
+        {
+            this.Mother = null;
+            this.Father = null;
+            this.Name = name;
+            this.Sex = sex;
+        }
+
+        public AdultFamilyMember(string name, Gender sex, FamilyMember mother, FamilyMember father)
+        {
+            this.Mother = mother;
+            this.Father = father;
+            this.Name = name;
+            this.Sex = sex;
+        }
+        private List<FamilyMember> Children { get; set; } = new List<FamilyMember>();
+        private AdultFamilyMember Spouse;
+        public override void Print(int indent = 0)
+        {
+            base.Print(indent);
+
+            foreach (var child in Children)
+                child.Print(indent * 2);
+        }
+        public override void Info(int indent = 0)
+        {
+            if (Spouse != null)
+            {
+                Console.WriteLine($"{new String('-', indent)}Имя {this.Name} | Супруг(а) {Spouse.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{new String('-', indent)}Имя {this.Name}");
+            }
+        }
+
+        public void AddChildren(FamilyMember children)
+        {
+            this.Children.Add(children);
+        }
+
+        public void AddSpouse(AdultFamilyMember spouse)
+        {
+            this.Spouse = spouse;
+        }
+
+    }
+}
---
+++ HW_GB_OOP_001/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HW_GB_OOP_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var grandMother1 = new AdultFamilyMember("Maria", Gender.Female);
+            var grandFather1 = new AdultFamilyMember("Petr", Gender.Male);
+            var grandMother2 = new AdultFamilyMember("Svetlana", Gender.Female);
+            var grandFather2 = new AdultFamilyMember("Igor", Gender.Male);
+            var mother = new AdultFamilyMember("Elena", Gender.Female, grandMother1, grandFather1);
+            var father = new AdultFamilyMember("Vasiliy", Gender.Male, grandMother2, grandFather2);
+            var son = new FamilyMember("Ivan", Gender.Male, mother, father);
+
+            grandMother1.AddChildren(mother);
+            grandMother2.AddChildren(father);
+            grandFather1.AddChildren(mother);
+            grandFather2.AddChildren(father);
+            mother.AddChildren(son);
+            father.AddChildren(son);
+            grandFather1.AddSpouse(grandMother1);
+            mother.AddSpouse(father);
+
+            grandFather1.Print(1);
+
+        }
+    }
+}
